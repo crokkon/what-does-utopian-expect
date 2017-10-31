cbc479cbf1f1af5398064c68e381ddd3fb2dcbcb
crokkon <33018033+crokkon@users.noreply.github.com>
2017-10-31 20:18:20 +0000

    Update query-stats.js

--- query-stats.js
+++ query-stats.js
@@ -1,1 +1,6 @@
-document.write("Hello World!");
+var url = "https://api.utopian.io/api/stats";
+var xhr = new XMLHttpRequest();
+xhr.open("GET", url, false);
+xhr.send();
+var result = JSON.parse(xhr.response);
+document.write(results);
