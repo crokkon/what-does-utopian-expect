aecc5770714f83568cee558a492dccf79dd3fa22
crokkon <33018033+crokkon@users.noreply.github.com>
2017-10-31 20:39:32 +0000

    Update query-stats.js

--- query-stats.js
+++ query-stats.js
@@ -1,4 +1,11 @@
 var url = "https://api.utopian.io/api/stats";
+
+function colHeader(var title) {
+  return "<th scope=\"col\">" + title + "</th>";
+
+function tblCell(var content) {
+  return "<td>" + content + "</td>";
+  
 var xhr = new XMLHttpRequest();
 xhr.open("GET", url, false);
 xhr.send();
@@ -6,8 +13,9 @@ var results = JSON.parse(xhr.response);
 var stats = results.stats;
 var votes = stats.votes;
 var categories = stats.categories;
+document.write("<table class="table"><thead><tr>" + colHeader("Category) + colHeader("Total Posts") + </tr></thead><tbody>");
 for (var key in categories) {
   c = categories[key];
-  var line = "<p>Category :" + key + " total_posts: " + c.total_posts + "</p>";
-  document.write(line);
+  document.write("<tr><th scope=\"row\">" + key + "</th>" + tblCell(c.total_posts) + "</tr>");
 }
+document.write("</tbody></table>");
